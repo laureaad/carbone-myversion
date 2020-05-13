345af9efc10031e7404d3f674c2c738f520ed410
laureaad <65254063+laureaad@users.noreply.github.com>
2020-05-13 12:49:52 -0300

    Update _currency.js

--- formatters/_currency.js
+++ formatters/_currency.js
@@ -3,8 +3,8 @@
 // https://github.com/mledoze/countries
 
 module.exports = {
-  EUR : { major : 'euro'   , symbol : '€'  , precision : 2 , minor : 'cent'  , minSymbol : 'c'  , name : 'Euros'         },
-  USD : { major : 'dollar' , symbol : '$'  , precision : 2 , minor : 'cent'  , minSymbol : '¢'  , name : 'US Dollar'     },
-  GBP : { major : 'pound'  , symbol : '£'  , precision : 2 , minor : 'penny' , minSymbol : 'p'  , name : 'British Pound' },
-  CHF : { major : 'franc'  , symbol : 'CHF', precision : 2 , minor : 'rappen', minSymbol : 'Rp.', name : 'Swiss Franc'   }
+  EUR : { major : 'euro'   , symbol : ''  , precision : 2 , minor : 'cent'  , minSymbol : 'c'  , name : 'Euros'         },
+  USD : { major : 'dollar' , symbol : ''  , precision : 2 , minor : 'cent'  , minSymbol : '¢'  , name : 'US Dollar'     },
+  GBP : { major : 'pound'  , symbol : ''  , precision : 2 , minor : 'penny' , minSymbol : 'p'  , name : 'British Pound' },
+  CHF : { major : 'franc'  , symbol : '', precision : 2 , minor : 'rappen', minSymbol : 'Rp.', name : 'Swiss Franc'   }
 };
